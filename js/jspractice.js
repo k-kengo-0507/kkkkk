d28ad3f52c322376a99aaac86ef185a80a3fd241
kengo0507 <k0507k@gamil.com>
2020-07-17 11:30:49 +0900

    js basic①学習

---
+++ js/jspractice.js
@@ -0,0 +1,18 @@
+document.write("HelloWorld");
+console.log("HelloWorld");
+alert("HelloWorld");
+
+console.log("kamada"+"kengo");
+console.log(5*5);
+console.log("5"+"7");
+
+let num = 1;
+num = ++num;
+console.log(num);
+
+let age =30;
+age =40;
+console.log(age);
+
+const age2 =30;
+console.log(age2);
